3130e86c37243b07674381044dbe3394c574ba92
Ripal Nathuji <rnathuji@neoteklabs.com>
2022-02-15 16:47:20 -0600

    Add utility methods to find media references

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,9 +3,32 @@ import html
 from mbtools import utils
 
 
-LESSON1_CONTENT1 = "<p>Lesson 1 content 1</p>"
-LESSON1_CONTENT2 = "<p>Lesson 1 content 2</p>"
-PAGE2_CONTENT = "<p>Page 2 content</p>"
+IM_MEDIA_LINK = "https://s3.amazonaws.com/im-ims-export/imagename"
+OSX_MEDIA_LINK = "https://osx-int-alg.s3.us-east-1.amazonaws.com/l1/imagename"
+MOODLE_VIDEO_FILE = "@@PLUGINFILE@@/video.mp4"
+MOODLE_TRACK_FILE = "@@PLUGINFILE@@/video.vtt"
+
+LESSON1_CONTENT1 = (
+    '<div>'
+    f'<img src="{IM_MEDIA_LINK}">'
+    '<img src="https://validsite/imagename">'
+    '</div>'
+)
+LESSON1_CONTENT2 = (
+    '<div>'
+    f'<img src="{OSX_MEDIA_LINK}">'
+    '</div>'
+)
+PAGE2_CONTENT = (
+    '<div>'
+    '<video controls="true">'
+    f'<source src="{MOODLE_VIDEO_FILE}">'
+    f'<track src="{MOODLE_TRACK_FILE}">'
+    '<track src="https://validsite/video.vtt">'
+    'fallback content'
+    '</video>'
+    '</div>'
+)
 
 
 @pytest.fixture
@@ -90,7 +113,29 @@ def test_parse_activity_html_contents(mbz_path):
     parsed_html_content_strings = []
     for act in activities:
         for html_elem in act.html_elements():
-            parsed_html_content_strings.append(html_elem.tostring())
+            parsed_html_content_strings.append(
+                html_elem.tostring().decode("utf-8")
+            )
 
     assert set([LESSON1_CONTENT1, LESSON1_CONTENT2, PAGE2_CONTENT]) == \
         set(parsed_html_content_strings)
+
+
+def test_find_external_media_references(mbz_path):
+    activities = utils.parse_backup_activities(mbz_path)
+    media_references = []
+
+    for act in activities:
+        media_references += utils.find_external_media_references(act)
+
+    assert set([IM_MEDIA_LINK, OSX_MEDIA_LINK]) == set(media_references)
+
+
+def test_find_moodle_media_references(mbz_path):
+    activities = utils.parse_backup_activities(mbz_path)
+    media_references = []
+
+    for act in activities:
+        media_references += utils.find_moodle_media_references(act)
+
+    assert set([MOODLE_VIDEO_FILE, MOODLE_TRACK_FILE]) == set(media_references)
