def365d8ffe293bbb77f0f0d3f68fb2d3606c493
Prabhdip Gill <pg53@rice.edu>
2022-09-13 09:06:48 -0700

    Fix branching code

--- mbtools/html_to_json.py
+++ mbtools/html_to_json.py
@@ -5,27 +5,13 @@ from pathlib import Path
 
 
 def create_json_content(uuid, content, json_path):
-    if not Path(json_path).exists():
 
-        json_content = {
-            "id": uuid,
-            "content": content}
+    json_content = {
+        "id": uuid,
+        "content": content}
 
-        with open(json_path, "w") as new_file:
-            new_file.write(json.dumps(json_content, indent=2))
-    else:
-
-        with open(json_path) as json_file:
-            file_content = json_file.read()
-            if file_content:
-
-                content_from_json = json.loads(file_content)["content"]
-
-        combined_content = content_from_json + content
-
-        json_content = {"id": uuid, "content": combined_content}
-        with open(json_path, "w") as new_file:
-            new_file.write(json.dumps(json_content, indent=2))
+    with open(json_path, "w") as new_file:
+        new_file.write(json.dumps(json_content, indent=2))
 
 
 def html_to_json(html_directory, json_directory):
@@ -34,35 +20,27 @@ def html_to_json(html_directory, json_directory):
 
     for file in Path(html_directory).iterdir():
         if Path(f"{html_directory}/{file.name}").is_file():
-
             file_uuid = f"{Path(file.name).stem}"
             file_content = ""
+            variant_list = []
 
             with open(f"{html_directory}/{file.name}") as f:
                 file_content = f.read()
+                variant_list.append({"variant": "main", "html": file_content})
 
+            if Path(f'{html_directory}/{file.stem}').is_dir():
+                for f_name in Path(f"{html_directory}/{file.stem}").iterdir():
+                    with open(f"{html_directory}/"
+                              f"{file.stem}/{f_name.name}") as varient_file:
+
+                        variant_list.append(
+                            {
+                                "variant": f"{Path(f_name.name).stem}",
+                                "html": varient_file.read()
+                            }
+                        )
             create_json_content(
-                file_uuid,
-                [{"variant": "main", "html": file_content}],
-                f"{json_directory}/" f"{Path(file.name).stem}.json",
-            )
-
-        elif Path(f'{html_directory}/{file.name}.html').exists():
-            variant_list = []
-            for f_name in Path(f"{html_directory}/{file.name}").iterdir():
-                with open(f"{html_directory}/"
-                          f"{file.name}/{f_name.name}") as varient_file:
-
-                    variant_list.append(
-                        {
-                            "variant": f"{Path(f_name.name).stem}",
-                            "html": varient_file.read()
-                        }
-                    )
-            if variant_list:
-
-                create_json_content(
-                    Path(file.name).stem,
+                    file_uuid,
                     variant_list,
                     f"{json_directory}/" f"{Path(file.name).stem}.json",
                 )
@@ -85,4 +63,5 @@ def main():
 
 
 if __name__ == "__main__":  # pragma: no cover
-    main()
+    # main()
+    html_to_json('./html', './json')
