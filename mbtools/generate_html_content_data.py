e960d4e6c7ff900e63cc38a44e9921ee4ab27e33
Prabhdip Gill <pg53@rice.edu>
2023-02-28 09:45:11 -0800

    Address review comments

--- mbtools/generate_html_content_data.py
+++ mbtools/generate_html_content_data.py
@@ -2,29 +2,27 @@ import argparse
 from pathlib import Path
 from bs4 import BeautifulSoup
 import csv
+from mbtools import utils
 
-CLASSES_TO_INJECT = ["os-raise-ib-input", "os-raise-ib-pset-problem"]
+CLASSES_TO_COLLECT = ["os-raise-ib-input", "os-raise-ib-pset-problem"]
 
 
-def inject_ib_uuids(html_dir, output_path):
-    ib_input_instances_dict = []
-    ib_pset_problems_dict = []
+def generate_html_content_data(html_dir, output_path):
+    ib_input_instances_list = []
+    ib_pset_problems_list = []
     for path in Path(html_dir).rglob("*.html"):
 
         with open(path, "r") as f:
             soup = BeautifulSoup(f.read(), "html.parser")
-            for tag in CLASSES_TO_INJECT:
+            for tag in CLASSES_TO_COLLECT:
                 for elem in soup.find_all(class_=tag):
+                    (content_id, variant) = parse_content_id_and_variant(path)
                     if tag == "os-raise-ib-input":
-                        ib_input_instances_dict.append(
+                        ib_input_instances_list.append(
                             {
                                 "id": elem["data-content-id"],
-                                "content_id": path.as_posix()[
-                                    path.as_posix().find("html/")
-                                    + 5: path.as_posix().find("html/")
-                                    + 36
-                                ],
-                                "variant": Path(path).stem,
+                                "content_id": content_id,
+                                "variant": variant,
                                 "content": elem.find(
                                     name="div",
                                     class_="os-raise-ib-input-content"
@@ -36,15 +34,11 @@ def inject_ib_uuids(html_dir, output_path):
                             }
                         )
                     if tag == "os-raise-ib-pset-problem":
-                        ib_pset_problems_dict.append(
+                        ib_pset_problems_list.append(
                             {
                                 "id": elem["data-content-id"],
-                                "content_id": path.as_posix()[
-                                    path.as_posix().find("html/")
-                                    + 5: path.as_posix().find("html/")
-                                    + 36
-                                ],
-                                "variant": Path(path).stem,
+                                "content_id": content_id,
+                                "variant": variant,
                                 "content": elem.find(
                                     name="div",
                                     class_="os-raise-ib-pset-problem-content",
@@ -52,35 +46,34 @@ def inject_ib_uuids(html_dir, output_path):
                                 "pset_id": elem.parent["data-content-id"],
                                 "problem_type": elem["data-problem-type"],
                                 "solution": elem["data-solution"],
-                                "solution_options":
-                                elem["data-solution-options"]
-                                if elem["data-problem-type"] != "input"
-                                else "",
+                                "solution_options": elem.get(
+                                    "data-solution-options", ""
+                                    )
                             }
                         )
 
-    with open(f"{output_path}/ib_input_instances.csv", "w") as myFile:
-        writer = csv.writer(myFile)
-        writer.writerow(["id", "content_id", "variant", "content", "prompt"])
-        for dictionary in ib_input_instances_dict:
-            writer.writerow(dictionary.values())
+    with open(f"{output_path}/ib_input_instances.csv", "w") as input_outfile,\
+         open(f"{output_path}/ib_pset_problems.csv", "w") as pset_outfile:
+        input_instance_headers = ib_input_instances_list[0].keys()
+        input_result = csv.DictWriter(
+            input_outfile,
+            fieldnames=input_instance_headers)
+        input_result.writeheader()
+        input_result.writerows(ib_input_instances_list)
 
-    with open(f"{output_path}/ib_pset_problems.csv", "w") as myFile:
-        writer = csv.writer(myFile)
-        writer.writerow(
-            [
-                "id",
-                "content_id",
-                "variant",
-                "content",
-                "pset_id",
-                "problem_type",
-                "solution",
-                "solution_options",
-            ]
-        )
-        for dictionary in ib_pset_problems_dict:
-            writer.writerow(dictionary.values())
+        pset_headers = ib_pset_problems_list[0].keys()
+        pset_result = csv.DictWriter(pset_outfile, fieldnames=pset_headers)
+        pset_result.writeheader()
+        pset_result.writerows(ib_pset_problems_list)
+
+
+def parse_content_id_and_variant(html_path):
+    uuid_or_variant = f"{Path(html_path.name).stem}"
+    if utils.validate_uuid4(uuid_or_variant):
+        return (uuid_or_variant, "main")
+    else:
+        content_id = html_path.parent.stem
+        return (content_id, uuid_or_variant)
 
 
 def main():
@@ -96,7 +89,7 @@ def main():
     html_directory = Path(args.html_directory).resolve(strict=True)
     output_directory = Path(args.output_directory).resolve(strict=True)
 
-    inject_ib_uuids(html_directory, output_directory)
+    generate_html_content_data(html_directory, output_directory)
 
 
 if __name__ == "__main__":  # pragma: no cover
--- tests/test_generate_html_content_data.py
+++ tests/test_generate_html_content_data.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from mbtools import generate_html_content_data
 import pytest
+import csv
 
 
 @pytest.fixture
@@ -10,66 +11,51 @@ def test_data_path():
 
 
 @pytest.fixture
-def html_content(test_data_path):
-    with open(test_data_path /
-              "4ca6ab6d-231e-4000-b576-af7f3c021cf5.html", "r") as f:
+def content(test_data_path):
+    with open(test_data_path / "4ca6ab6d-231e-4000-b576-af7f3c021cf5.html",
+                               "r") as f:
         main_variant = f.read()
     with open(
-        test_data_path /
-        "4ca6ab6d-231e-4000-b576-af7f3c021cf5" / "mm.html", "r"
-    ) as f:
+        test_data_path / "4ca6ab6d-231e-4000-b576-af7f3c021cf5" / "mm.html",
+                         "r") as f:
         variant = f.read()
 
-    with open(test_data_path / "ib_input_instances_expected.csv", "r") as f:
-        input_expected_csv = f.read()
-    with open(test_data_path / "ib_pset_problems_expected.csv", "r") as f:
-        pset_expected_csv = f.read()
+    input_expected_csv = csv.DictReader(
+        open(test_data_path / "ib_input_instances_expected.csv", "r")
+    )
+    pset_expected_csv = csv.DictReader(
+        open(test_data_path / "ib_pset_problems_expected.csv", "r")
+    )
 
     return (main_variant, variant, input_expected_csv, pset_expected_csv)
 
 
-def test_generate_html_content(tmp_path, mocker, html_content):
-    (main_variant,
-     variant,
-     input_expected_csv,
-     pset_expected_csv) = html_content
+def test_generate_html_content(tmp_path, mocker, content):
+    (main_variant, variant, input_expected_csv, pset_expected_csv) = content
 
     os.mkdir(tmp_path / "html")
+    os.mkdir(tmp_path / "output")
     os.mkdir(tmp_path / "html/4ca6ab6d-231e-4000-b576-af7f3c021cf5")
 
     html_dir = f"{tmp_path}/html"
     main_path = Path(html_dir) / "4ca6ab6d-231e-4000-b576-af7f3c021cf5.html"
-    variant_path = Path(html_dir) / "4ca6ab6d-231e-4000"\
-        "-b576-af7f3c021cf5" / "mm.html"
+    variant_path = (
+        Path(html_dir) / "4ca6ab6d-231e-4000" "-b576-af7f3c021cf5" / "mm.html"
+    )
     main_path.write_text(main_variant)
     variant_path.write_text(variant)
-    input_expected = Path(html_dir) / "ib_input_instances_expected.csv"
-    pset_expected = Path(html_dir) / "ib_pset_problems_expected.csv"
-    input_expected.write_text(input_expected_csv)
-    pset_expected.write_text(pset_expected_csv)
 
-    mocker.patch("sys.argv", ["", html_dir, html_dir])
+    mocker.patch("sys.argv", ["", html_dir, f"{tmp_path}/output"])
 
     generate_html_content_data.main()
 
-    with open(
-        Path(html_dir) / "ib_input_instances_expected.csv"
-    ) as expected_ib_inputs, open(
-        Path(html_dir) / "ib_input_instances.csv"
-    ) as ib_inputs:
-        actual = ib_inputs.readlines()
-        expected = expected_ib_inputs.readlines()
-        for line in expected:
-            assert line in actual
-        assert len(actual) == len(expected)
+    actual_ib_input = csv.DictReader(open(Path(f"{tmp_path}/output") /
+                                     "ib_input_instances.csv"))
+    for line_expected, line_actual in zip(input_expected_csv, actual_ib_input):
+        assert line_expected == line_actual
 
-    with open(
-        Path(html_dir) / "ib_pset_problems_expected.csv"
-    ) as expected_pset_problems, open(
-        Path(html_dir) / "ib_pset_problems.csv"
-    ) as ib_psets:
-        actual = ib_psets.readlines()
-        expected = expected_pset_problems.readlines()
-        for line in expected:
-            assert line in actual
-        assert len(actual) == len(expected)
+    actual_ib_pset = csv.DictReader(open(Path(f"{tmp_path}/output") /
+                                    "ib_pset_problems.csv"))
+
+    for line_expected, line_actual in zip(pset_expected_csv, actual_ib_pset):
+        assert line_expected == line_actual
